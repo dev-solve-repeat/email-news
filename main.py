e37e97f92452deacff7478fcf0d0d6b0f6a79fac
Brijesh Babu <brijesh.babutv@gmail.com>
2022-12-21 00:09:47 +0530

    Day 28: Add links, specify language, restrict news to first 20, Add subject to the mail and control category

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import requests
 from send_email import send_emails
 
+topic = "tesla"
+# f should be added to the line where change needs to be made.
 api_key = "3017c762b3f9426191584b31895aa972"
-url = "https://newsapi.org/v2/everything?q=tesla&" \
-      "from=2022-11-20&sortBy=publishedAt&apiKey=" \
-      "3017c762b3f9426191584b31895aa972"
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2022-11-20&" \
+      "sortBy=publishedAt&" \
+      "apiKey=3017c762b3f9426191584b31895aa972&" \
+      "language=en" # for selecting the language
 
 # Get the request from the url
 request = requests.get(url)
@@ -14,10 +19,14 @@ content = request.json()
 
 # Access the article title and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]: # for restricting to 20 news
     if article["title"] is not None:   # this is to remove the error is title value is None -
         # which is a None type in python
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        # To add subject to the mail.
+        body = "Subject: Todays News" + "\n" + \
+               body + article["title"] + "\n"\
+               + article["description"] + "\n"\
+               + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_emails(message=body)
